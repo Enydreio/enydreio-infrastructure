a52d6330520e60a741c83ca927b6a2dd1c10da65
mschablinger <m.schablinger@students.htl-leonding.ac.at>
2025-02-28 11:09:14 +0100

    Tried to fix deployment

---
+++ entrypoint.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/bash
+
+wget -O app https://github.com/Enydreio/enydreio-backend/releases/download/main/enydreio-backend
+wget https://github.com/Enydreio/enydreio-frontend/releases/download/main/dist.tar.gz
+tar -xzf dist.tar.gz -C dist
+rm dist.tar.gz
+chmod +x app
