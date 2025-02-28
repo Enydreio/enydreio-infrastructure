c58db205326dd0f320f65b3e9c0163bff0d4707e
mschablinger <m.schablinger@students.htl-leonding.ac.at>
2025-02-28 11:26:57 +0100

    Fixed deployment

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 wget -O app https://github.com/Enydreio/enydreio-backend/releases/download/main/enydreio-backend
 wget https://github.com/Enydreio/enydreio-frontend/releases/download/main/dist.tar.gz
+mkdir dist
 tar -xzf dist.tar.gz -C dist
 rm dist.tar.gz
 chmod +x app
