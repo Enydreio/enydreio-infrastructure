c5e7705a2c8d658cf908d0388ce81cd1a5fe9d73
mschablinger <m.schablinger@students.htl-leonding.ac.at>
2025-03-22 16:18:03 +0100

    Built workaround for finding host ip

--- start-docker.sh
+++ start-docker.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-
-export HOST_IP=$(ip route get 1.1.1.1 | awk '{print $7}') && docker-compose --env-file ./.env up -d
+cp .env .env.temp
+echo "HOST_IP=$(ip route get 1.1.1.1 | awk '{print $7}')" >> .env.temp
+docker compose --env-file ./.env.temp up -d
+rm .env.temp
